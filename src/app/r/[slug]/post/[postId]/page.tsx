8c455a4e623fd8e2cb99aa4126f097fce514be24
Dwight Mcleish Jr <dmcleishjr@gmail.com>
2023-08-05 23:18:08 -0400

    feat: adding support for rendering posts

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,6 +1,12 @@
+import PostVoteServer from '@/components/post-vote/postvoteserver';
+import { buttonVariants } from '@/components/ui/button';
+import { db } from '@/lib/db';
 import { redis } from '@/lib/redis';
 import { CachePost } from '@/types/redis-types';
 import { Post, User, Vote } from '@prisma/client';
+import { ArrowBigDown, ArrowBigUp, Loader2 } from 'lucide-react';
+import { notFound } from 'next/navigation';
+import { Suspense } from 'react';
 
 type PageProps = {
   params: {
@@ -17,7 +23,59 @@ export default async function Page({ params }: PageProps) {
   let post: (Post & { votes: Vote[]; author: User }) | null = null;
 
   if (!cachedPost) {
+    post = await db.post.findFirst({
+      where: {
+        id: params.postId,
+      },
+      include: {
+        votes: true,
+        author: true,
+      },
+    });
   }
 
-  return <div>page</div>;
+  if (!post && !cachedPost) return notFound();
+
+  return (
+    <div>
+      <div className='h-full flex flex-col sm:flex-row items-center sm:items-start justify-between'>
+        <Suspense fallback={<PostVoteShell />}>
+          <PostVoteServer
+            postId={post?.id ?? cachedPost.id}
+            getData={async () => {
+              return await db.post.findUnique({
+                where: {
+                  id: params.postId,
+                },
+                include: {
+                  votes: true,
+                },
+              });
+            }}
+          />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
+
+function PostVoteShell() {
+  return (
+    <div className='flex items-center flex-col pr-6 w-20'>
+      {/*Upvote*/}
+      <div className={buttonVariants({ variant: 'ghost' })}>
+        <ArrowBigUp className='h-5 w-5 text-zinc-700' />
+      </div>
+
+      {/*Score*/}
+      <div className='text-center py-2 font-medium text-sm text-zinc-900'>
+        <Loader2 className='h3 w-3 animate-spin' />
+      </div>
+
+      {/*Downvote*/}
+      <div className={buttonVariants({ variant: 'ghost' })}>
+        <ArrowBigDown className='h-5 w-5 text-zinc-700' />
+      </div>
+    </div>
+  );
 }
--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button, buttonVariants } from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
